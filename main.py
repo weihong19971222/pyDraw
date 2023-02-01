0959843512d49efd753e1175853665d88a0c6bd0
weihong19971222 <46885087+weihong19971222@users.noreply.github.com>
2023-02-01 12:02:12 +0800

    selenium

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+from selenium import webdriver
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+try:
+    driver = webdriver.Chrome('chromedriver')
+    url = 'https://www.google.com.tw'
+    driver.get(url)
+except Exception as e:
+    print(e)
+# def print_hi(name):
+#     print(f'Hi, {name}')
+#
+#
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
