9c27bf44fbed99fcae6eb89aa78b68952d425c2a
weihong19971222 <46885087+weihong19971222@users.noreply.github.com>
2023-02-18 09:48:34 +0800

    bu打卡

--- autoPickCard.py
+++ autoPickCard.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+import random
 
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
@@ -40,3 +41,39 @@ def outWork():
     driver.find_element(By.XPATH, '//*[@id="punchOut"]').click()
     # driver.close()
 
+def onWorkBu(date,res):
+    # 上班補打卡
+    driver.get("http://35.194.135.26/Gaia/public/applyRecord")
+    # 日期
+
+    # ((JavascriptExecutor) driver).executeScript("document.getElementsByName('date'[0].removeAttribute('readonly');");
+    # WebElement
+    # dateFld = driver.findElement(By.id("date_completed"));
+    # dateFld.clear();
+    # dateFld.sendKeys("date Completed");
+
+    dataPick = driver.find_element(By.XPATH, '//*[@id="day"]')
+    dataPick.click()
+    # dataPick.send_keys(date)
+    dataPick.send_keys(Keys.CONTROL, "a")  # Select all pre-existing text/input value
+    dataPick.send_keys(Keys.BACKSPACE)  # Remove that text
+    dataPick.send_keys("2022/01/01")
+    # 選擇時間
+    min = str(random.randint(1,10)).zfill(2)
+    driver.find_element(By.XPATH, '//*[@id="type"]').send_keys('上班')
+    driver.find_element(By.XPATH, '//*[@id="hour"]').send_keys('9')
+    driver.find_element(By.XPATH, '//*[@id="mins"]').send_keys(min)
+    # 原因
+    driver.find_element(By.XPATH, '/html/body/div/div[2]/div[2]/form/div[3]/textarea').send_keys(res)
+    # driver.close()
+
+def outWorkBu(date,res):
+    # 下班補打卡
+    driver.get("http://35.194.135.26/Gaia/public/applyRecord")
+    # 選擇時間
+    min = str(random.randint(11, 25))
+    driver.find_element(By.XPATH, '//*[@id="type"]').send_keys('下班')
+    driver.find_element(By.XPATH, '//*[@id="hour"]').send_keys('18')
+    driver.find_element(By.XPATH, '//*[@id="mins"]').send_keys(min)
+    # 原因
+    driver.find_element(By.XPATH, '/html/body/div/div[2]/div[2]/form/div[3]/textarea').send_keys(res)
--- main.py
+++ main.py
@@ -13,14 +13,17 @@ class PickCardView(QMainWindow,Ui_Dialog):
 
     def setupUi(self, Dialog):
         super().setupUi(Dialog)
-        Dialog.setFixedSize(301, 201)
+        Dialog.setFixedSize(301, 320)
         self.onWorkBtn.clicked.connect(self.onWorkPick)
         self.outWorkBtn.clicked.connect(self.outWorkPick)
+        self.onWorkbuBtn.clicked.connect(self.onWorkbu)
+        self.outWorkbuBtn.clicked.connect(self.outWorkbu)
 
     def retranslateUi(self, Dialog):
         super().retranslateUi(Dialog)
         self.nameEdt.setText(os.getenv("NAME"))
         self.passwordEdt.setText(os.getenv("PASS"))
+        self.bures.setText("忘了")
 
     def onWorkPick(self):
         loginStatus = AutoLogin(self.nameEdt.text().replace(" ", ""), self.passwordEdt.text().replace(" ", ""))
@@ -32,6 +35,14 @@ class PickCardView(QMainWindow,Ui_Dialog):
         if loginStatus:
             outWork()
 
+    def onWorkbu(self):
+        loginStatus = AutoLogin(self.nameEdt.text().replace(" ", ""), self.passwordEdt.text().replace(" ", ""))
+        if loginStatus:
+            onWorkBu(self.budate.text(),self.bures.text())
+    def outWorkbu(self):
+        loginStatus = AutoLogin(self.nameEdt.text().replace(" ", ""), self.passwordEdt.text().replace(" ", ""))
+        if loginStatus:
+            outWorkBu(self.budate.text(), self.bures.text())
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
